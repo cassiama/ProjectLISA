e2f85b7003dc493525af939855730a986046a083
cassiama <cassiama@stevens.edu>
2024-02-05 22:36:06 -0500

    server can display handlebars now

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from 'express';
 import { configRoutes } from './routes/index.js';
+import { engine } from 'express-handlebars';
+const staticDir = express.static(__dirname + '/public')
 
 const app = express();
+
+app.use('/public', staticDir);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.engine('handlebars', engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
