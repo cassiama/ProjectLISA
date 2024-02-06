5e0fe1e25f78042fd2ea73a3f7b998d5d081dd1a
Kashmeeber <areeb.ch2001@gmail.com>
2024-02-06 12:20:26 -0500

    Added data and helper functions

--- data/users.js
+++ data/users.js
@@ -1,14 +1,144 @@
-import { users, tickets } from "./mongoCollections.js";
-import { ObjectId } from "mongodb";
+import { checkName, checkEmail, checkPassword, checkString, checkId } from "../utils/helpers";
+import { users } from "../config/mongoCollections";
+import { ObjectId } from 'mongodb';
+import bcrypt from 'bcrypt';
+const saltRounds = 8;
 
 
-export const registerUser = async (firstName, lastName, email, password) => {};
+export const registerUser = async (firstName, lastName, email, password) => {
+    if (!firstName || !lastName || !email || !password) {
+        throw 'All input fields must be provided (registerUser)';
+    }
+    firstName = checkName(firstName, "First name");
+    lastName = checkName(lastName, "Last name");
+    emailAddress = checkEmail(emailAddress);
+    password = checkPassword(password);
+    let emailExists = await emailAlreadyExists(emailAddress);
+    if (emailExists) {
+        throw `emailAddress already exists (createUser)`;
+    }
+    const hashed = await bcrypt.hash(password, saltRounds);
+    let newUser = {
+        firstName: firstName,
+        lastName: lastName,
+        email: email,
+        password: hashed,
+		devices: [],
+		points: 0
+    };
+    const userCollection = await users();
+    const newInsertInformation = await userCollection.insertOne(newUser);
+    if (!newInsertInformation.insertedId) {
+        throw 'Insert failed!';
+    }
+    return await getUserById(newInsertInformation.insertedId.toString());
+};
 
-export const checkUser = async (email, password) => {};
+export const emailAlreadyExists = async (email) => {
+    if (!email) {
+      throw `Error: email must be inputed`;
+    }
+    email = checkEmail(email);
+  
+    const userCollection = await users();
+    const user = await userCollection.findOne({ email: email });
+    if (!user) {
+      return false;
+    } else {
+      return true;
+    }
+};
 
-export const updateUser = async (id, firstName, lastName, email, password) => {};
+export const checkUser = async (email, password) => {
+    if (!email || !password) {
+        throw "All input fields must be provided";
+    }
+    
+    email = checkEmail(email);
+    password = checkString(password, "Password");
 
-export const deleteUser = async id => {};
+    const userCollection = await users();
+
+    const user = await userCollection.findOne({email: email});
+    let validPassword = await bcrypt.compare(password, user.password);
+    if (validPassword) {
+        let firstName = user.firstName;
+        let lastName = user.lastName;
+        let emailAddress = user.email;
+        let _id = user._id.toString();
+        return {_id, firstName, lastName, emailAddress};
+    } else {
+        throw "Either the email address or password is invalid";
+    }
+};
+
+export const updateUser = async (id, firstName, lastName, email, password) => {
+	let hashed1;
+	if (id) {
+		id = checkId(id);
+	} else {
+		throw `Error: No id given`;
+	}
+	if (firstName) {
+		firstName = checkName(firstName);
+	}
+	if (lastName) {
+		lastName = checkName(lastName);
+	}
+	if (email) {
+		email = checkEmail(email);
+	}
+	if (password) {
+		password = checkPassword(password);
+		hashed1 = await bcrypt.hash(password, saltRounds);
+	}
+
+	let oldUser = await getUserById(id);
+	let emailExists = false;
+
+	if (oldUser.email !== email && email) {
+		emailExists = await emailAlreadyExists(emailAddress);
+		if (emailExists) {
+			throw `Email already exists (updateUser)`
+		}
+	}
+
+	const userUpdate = {
+		firstName: firstName ? firstName : oldUser.firstName,
+		lastName: lastName ? lastName : oldUser.lastName,
+		email: email ? email : oldUser.email,
+		password: hashed1 ? hashed1 : oldUser.password,
+		username: username ? username : oldUser.username,
+		age: age ? age : oldUser.age,
+		devices: oldUser.devices,
+		points: oldUser.points
+	};
+
+	const userCollection = await users();
+
+	const updateInfo = await userCollection.replaceOne({_id: new ObjectId(id)}, userUpdate);
+
+	if (updateInfo.modifiedCount === 0) {
+		throw `At least one field must be different to successfully update user`;
+	}
+	let newInfo = await getUserById(id);
+	return newInfo;
+};
+
+export const deleteUser = async (id) => {
+	if (!id) {
+		throw `Error: Id must be inputed`;
+	  }
+	  id = checkId(id);
+	
+	  const userCollection = await users();
+	  const user = await userCollection.findOneAndDelete({ _id: new ObjectId(id) });
+	  if (deletionInfo.lastErrorObject.n === 0) {
+		throw `Error: Could not delete user with id of ${id}`;
+	  }
+	
+	  return { ...deletionInfo.value, deleted: true };
+};
 
 export const getAllUsers = async () => {
 	const userCollection = await users();
@@ -68,7 +198,7 @@ export const helpTicket = async (email, message) => {
 	return newTicket;
 };
 
-zzconst subtractPoints = async (userId, rewardPoints) => {
+export const subtractPoints = async (userId, rewardPoints) => {
 	const userCollection = await users();
 	const user = await getUserById(userId);
 	// check if the user has enough points to subtract
@@ -85,7 +215,7 @@ zzconst subtractPoints = async (userId, rewardPoints) => {
 	return true;
 };
 
-const addPoints = async (userId, rewardPoints) => {
+export const addPoints = async (userId, rewardPoints) => {
 	const userCollection = await users();
 	const user = await getUserById(userId);
 	// add the points
@@ -98,7 +228,7 @@ const addPoints = async (userId, rewardPoints) => {
 	return true;
 };
 
-const getTips = async () => {
+export const getTips = async () => {
 	// array of tips as a string
 	let tipArray = [
 		"Ask suppliers to email you a receipt instead of printing it out",
@@ -123,11 +253,3 @@ const getTips = async () => {
 	return randomTips;
 };
 
-export {
-	getAllUsers,
-	getUserById,
-	helpTicket,
-	subtractPoints,
-	addPoints,
-	getTips,
-};
