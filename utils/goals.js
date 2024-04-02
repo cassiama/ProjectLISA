23d0c62a3ea76d862a26f79c8244ba8ffed29d7c
cassiama <cassiama@stevens.edu>
2024-04-02 13:52:06 -0400

    added weekly goal

--- utils/goals.js
+++ utils/goals.js
@@ -1,11 +1,12 @@
 const dailyGoals = [
     "Recharge before device reaches 20% (Daily)",
     "Clean out inbox (Daily)",
-    "Shut Down when not in use (Daily)"
+    "Shut Down when not in use (Daily)",
 ];
 const weeklyGoals = [
     "Download instead of streaming (Weekly)",
-    "Reduce screen time by 5 hours (Weekly)"
+    "Reduce screen time by 5 hours (Weekly)",
+    "Remove 100 MB of storage space (Weekly)",
 ];
 
 export const allGoals = [...dailyGoals, ...weeklyGoals];
